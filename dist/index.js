2fe9b5b86f20c031d614e31a61dd8932434d9a45
Eliran Tutia <eliran.tutia@is.com>
2022-01-13 19:14:22 +0200

    a

--- dist/index.js
+++ dist/index.js
@@ -8557,13 +8557,16 @@ try {
     const reviewers = chooseReviewer(c, amountOfReviewers);
     console.log(`Chosen reviewers: ${reviewers.map(r => r.username)}`);
 
-    const octokit = new github.getOctokit(token);
+    const octokit = github.getOctokit(token);
     const context = github.context;
 
     if (context.payload.pull_request == null) {
         core.setFailed("Pull request not found");
         return;
     }
+
+    console.log('octokit', octokit);
+    console.log('context', context);
 
     const pullRequestNumber = context.payload.pull_request.number;
     const params = {
